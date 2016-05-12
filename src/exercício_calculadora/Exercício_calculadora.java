df853b3f9ccf8e48afe05d25719280e873ff846a
KaioTM <kaioteixeira@id.uff.br>
2016-05-12 13:12:05 -0300

    Alteração do método Menu
    Alteração do mecanismo de funcionamento do método "Menu"

--- src/exercício_calculadora/Exercício_calculadora.java
+++ src/exercício_calculadora/Exercício_calculadora.java
@@ -12,66 +12,73 @@ import java.util.Scanner;
  * @author kaio.teixeira
  */
 public class Exercício_calculadora {
-
+    static final int soma = 1;
+    static final int subtrai = 2;
+    static final int multiplica = 3;
+    static final int divide = 4;
+    static final int limpa = 5;
+    static final int sai = 6;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        menu();
-    }
-    
-    public static void menu(){
-        int opcao = 0;
-        int estado_memoria = 0;
+        int opcao,estado_memoria = 0;
         
         do{
             System.out.println("Estado da memória: "+ estado_memoria);
-            System.out.println("Opções: ");
-            System.out.println("\n(1) Somar");
-            System.out.println("(2) Subtrair");
-            System.out.println("(3) Multiplicar");
-            System.out.println("(4) Dividir");
-            System.out.println("(5) Limpar memória");
-            System.out.println("(6) Sair do Programa");
-            
-            Scanner ler = new Scanner(System.in);
-            opcao = ler.nextInt();
-            
+            opcao=mostra_menu();
             switch (opcao){
                 
-                case 1:{
+                case soma:{
                     estado_memoria = soma(estado_memoria);
                     break;
                 }
                 
-                case 2:{
+                case subtrai:{
                     estado_memoria = subtracao(estado_memoria);
                     break;
                 }
                 
-                case 3:{
+                case multiplica:{
                     estado_memoria = multiplicacao(estado_memoria);
                     break;
                 }
                 
-                case 4:{
+                case divide:{
                     estado_memoria = divisao(estado_memoria);
                     break;
                 }
-                case 5:{
+                case limpa:{
                     estado_memoria = limpar();
                     break;
                 }
                 
-                case 6:{
+                case sai:{
                     break;
                 }
                 default:
                     System.out.println("Comando inválido! Miseravi");
             }
-  
-        } while (opcao!=6);
+            
+        }while (opcao!=6);
+       
+    }
+    
+    public static int mostra_menu(){
+        int opcao;
+        //System.out.println("Estado da memória: "+ estado_memoria);
+        System.out.println("Opções: ");
+        System.out.println("\n(1) Somar");
+        System.out.println("(2) Subtrair");
+        System.out.println("(3) Multiplicar");
+        System.out.println("(4) Dividir");
+        System.out.println("(5) Limpar memória");
+        System.out.println("(6) Sair do Programa");
+        System.out.print("Selecione: ");
+        Scanner ler = new Scanner(System.in);
+        opcao = ler.nextInt();
         
+        return opcao;
     }
     
     public static int soma(int estado_memoria){
